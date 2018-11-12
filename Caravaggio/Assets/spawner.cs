02473fc8cacb1d0996568138a9bcf9bf21545c75
J.v.Garderen <j.v.garderen93@gmail.com>
2018-11-12 14:51:38 +0100

    fixed some bugs

---
+++ Caravaggio/Assets/spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Spawn();
+        }
+	}
+
+    void Spawn()
+    {
+        GameObject instance = Instantiate(Resources.Load("art", typeof(GameObject))) as GameObject;
+    }
+}
