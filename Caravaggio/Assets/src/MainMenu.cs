45a2d52048c640cc08f152624197d4a78a156119
J.v.Garderen <j.v.garderen93@gmail.com>
2018-11-14 09:41:49 +0100

    WIP Menu's

---
+++ Caravaggio/Assets/src/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public float menuTimeVisable;
+    private IEnumerator coroutine;
+
+    // Use this for initialization
+    void Start () {
+        //coroutine = WaitAndPrint(menuTimeVisable);
+        //StartCoroutine(coroutine);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private IEnumerator WaitAndPrint(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            Debug.Log("Done");
+            //startMenuCanvas.SetActive(false);
+            StopCoroutine(coroutine);
+        }
+    }
+}
