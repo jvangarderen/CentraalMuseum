9e0307993db5f0fc542c0dd33dc19ff7dc74261b
AIEUtrecht <aiecozone.bnl@capgemini.com>
2018-11-21 14:19:41 +0100

    fixed rotation selection freeteleport

---
+++ Caravaggio/Assets/src/FoldingMuseum.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoldingMuseum : MonoBehaviour {
+
+	private List<GameObject> buildingParts;
+
+	// Use this for initialization
+	void Start () {
+		buildingParts = new List<GameObject>();
+		foreach (Transform child in transform) {
+			if(child.GetComponent<RotatingPart>()) {
+				buildingParts.Add(child.gameObject);
+				Debug.Log(buildingParts.Count);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.Space)) {
+			startFolding();
+		}
+	}
+
+	void startFolding()
+	{
+		for(int i =0; i <buildingParts.Count;i++) {
+			RotatingPart rp = buildingParts[i].GetComponent<RotatingPart>();
+			rp.startFolding();
+		}
+	}
+}
---
+++ Caravaggio/Assets/src/RotatingPart.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingPart : MonoBehaviour {
+	private Transform startTransform;
+	public enum Axis // your custom enumeration
+	{
+		x, y, z
+	};
+	public Axis axis;
+	public float rotationValue;
+	public float timeToFinishRotation;
+	private bool fold = false;
+	private float rotateSpeed;
+	void Start() {
+		startTransform = transform;
+		rotateSpeed = rotationValue / timeToFinishRotation;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		Rotate();
+	}
+
+	private void Rotate()
+	{
+		Vector3 rot = new Vector3();
+		switch (axis) {
+			case Axis.x: rot = new Vector3(transform.rotation.x + rotationValue, transform.rotation.y, transform.rotation.z); break;
+			case Axis.y: rot = new Vector3(transform.rotation.x, transform.rotation.y + rotationValue, transform.rotation.z); break;
+			case Axis.z: rot = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z + rotationValue); break;
+		}
+		Quaternion rotation = Quaternion.Euler(rot);
+		//transform.rotation = Quaternion.Lerp(transform.rotation,rotation, Time.deltaTime);
+
+
+
+		//example
+		// calculate rotation speed
+		float rotationSpeed = rotationValue / timeToFinishRotation;
+	}
+
+	public void startFolding()
+	{
+		fold = true;
+	}
+}
